4307071dff52108c56dc907cad4c2da6b60c0a4a
Callank21 <95372216+Callank21@users.noreply.github.com>
2022-05-19 15:54:06 -0700

    finished associations between the models

--- models/ProductTag.js
+++ models/ProductTag.js
@@ -28,17 +28,6 @@ ProductTag.init(
     }
   },
   {
-    id: {
-      type: DataTypes.INTEGER,
-      allowNull: false,
-      primaryKey: true,
-      autoIncrement: true
-    },
-    tag_name: {
-      type: DataTypes.STRING
-    }
-  },
-  {
     sequelize,
     timestamps: false,
     freezeTableName: true,
